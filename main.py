d9a092960a381f694b7306bbfe5db125d83bec03
Hamilton Mussi <hamiltonmussi.cf@gmail.com>
2025-05-14 19:36:51 -0300

    Refactor the whole structure

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
-from keygen import key_gen
-
-# Parameters definition (Kyber-512)
-k = 2  
-eta = 2
-q = 3329
-d_t = 11
+from models.kyber import Kyber
+from time import time
 
 if __name__ == "__main__":
-    public_key, secret_key = key_gen(k, eta, q, d_t)
-    t_compressed, rho = public_key
+    k = 2  
+    eta = 2
+    q = 3329
+    d_t = 11
 
-    print("Public Key (compressed t):", t_compressed)
-    print("Seed rho:", rho.hex())
-    print("Secret Key (s vector):", secret_key)
+    kyber_start_time = time()
 
-    with open("pub.key", 'wb') as f:
-        f.write(rho)
-        for poly in t_compressed:
-            for coeff in poly:
-                f.write(int(coeff).to_bytes(2, 'little'))
-    
-    with open("priv.key", 'wb') as f:
-        for poly in secret_key:
-            for coeff in poly:
-                f.write((int(coeff) % q).to_bytes(2, 'little'))
+    kyber = Kyber(k, eta, q, d_t)
+    kyber.save_keys()
+
+    kyber_end_time = time()
